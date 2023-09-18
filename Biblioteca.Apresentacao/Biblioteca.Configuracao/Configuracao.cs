aab121212fc3c4dd1644b69908500184e399e4d7
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2023-09-18 17:10:09 -0300

    Adicionando biblioteca de configuração

---
+++ Biblioteca.Apresentacao/Biblioteca.Configuracao/Configuracao.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Biblioteca.Configuracao
+{
+    public class Configuracao
+    {
+        public void Configurar(IServiceCollection servies)
+        {
+            new BancoDadosConfiguracao().Configurar(servies);
+            new RepositorioConfiguracao().Configurar(servies);
+            new ServicoConfiguracao().Configurar(servies);
+        }
+    }
+}
---
+++ Biblioteca.Apresentacao/Biblioteca.Configuracao/RepositorioConfiguracao.cs
@@ -0,0 +1,14 @@
+﻿using Biblioteca.Dominio.Repositorio;
+using Biblioteca.Repositorio;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Biblioteca.Configuracao
+{
+    internal class RepositorioConfiguracao
+    {
+        public void Configurar(IServiceCollection services)
+        {
+            services.AddTransient<ITurnoRepositorio, TurnoRepositorio>();
+        }
+    }
+}
