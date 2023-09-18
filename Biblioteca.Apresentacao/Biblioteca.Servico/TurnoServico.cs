48bdd0b566238fa67bb4bcdc62f4a6b571314edd
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2023-09-18 17:12:56 -0300

    Adicionando turno serviço

---
+++ Biblioteca.Apresentacao/Biblioteca.Servico/TurnoServico.cs
@@ -0,0 +1,47 @@
+﻿using Biblioteca.Dominio.Entidades;
+using Biblioteca.Dominio.Servico;
+
+namespace Biblioteca.Servico
+{
+    public class TurnoServico : ITurnoServico
+    {
+        public Dictionary<string, List<string>>? ValidarInserir(Turno turno)
+        {
+            var erros = new Dictionary<string, List<string>>();
+
+            if (string.IsNullOrEmpty(turno.Nome))
+            {
+                erros.Add("nome", new List<string>() { "Nome do turno é obrigatório" });
+            }
+
+            if (erros.Any())
+            {
+                return erros;
+            }
+
+            return null;
+        }
+
+        public Dictionary<string, List<string>>? ValidarEditar(Turno turno)
+        {
+            var erros = new Dictionary<string, List<string>>();
+
+            if (turno.IdTurno == null)
+            {
+                erros.Add("idTurno", new List<string> { "Id do turno é obrigatório" });
+            }
+
+            if (string.IsNullOrEmpty(turno.Nome))
+            {
+                erros.Add("nome", new List<string>() { "Nome do turno é obrigatório" });
+            }
+
+            if (erros.Any())
+            {
+                return erros;   
+            }
+
+            return null;
+        }
+    }
+}
