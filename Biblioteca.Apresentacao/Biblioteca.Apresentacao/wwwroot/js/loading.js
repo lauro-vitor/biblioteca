82b1f9bd12c1ef4bc2b5aab1dbf842608cdc72fe
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2023-09-21 16:44:11 -0300

    inserindo loading na tela inteira

--- Biblioteca.Apresentacao/Biblioteca.Apresentacao/wwwroot/js/loading.js
+++ Biblioteca.Apresentacao/Biblioteca.Apresentacao/wwwroot/js/loading.js
@@ -1,16 +1,16 @@
 const loading = {
-  bloquear: function () {
-    const spinner = $(
-      `<div id="idLoadingSpinner" class="d-flex justify-content-center loading-container">
-            <div class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>
-        </div>`
-    );
+    bloquear: function () {
+        const spinner = $(
+             `<div id="idLoadingSpinner" class="d-flex justify-content-center loading-container">
+               <div class="spinner-border" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+             </div>`
+        );
+        $("#idContainerLayout").append(spinner);
+    },
 
-    $("#idMainLayout").append(spinner);
-  },
-  desbloquear: function () {
-    $("#idLoadingSpinner").remove();
-  },
+    desbloquear: function () {
+        $("#idLoadingSpinner").remove();
+    },
 };
