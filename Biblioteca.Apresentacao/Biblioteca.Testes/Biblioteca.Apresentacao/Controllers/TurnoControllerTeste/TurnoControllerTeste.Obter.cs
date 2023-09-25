5d3c313208f13ee05170888ff0904dfedca6414d
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2023-09-25 18:38:30 -0300

    Testes TurnoController

---
+++ Biblioteca.Apresentacao/Biblioteca.Testes/Biblioteca.Apresentacao/Controllers/TurnoControllerTeste/TurnoControllerTeste.Obter.cs
@@ -0,0 +1,52 @@
+﻿using Biblioteca.Apresentacao.Controllers;
+using Biblioteca.Dominio.DTO;
+using Biblioteca.Testes.Mocks.Repositorio;
+using Biblioteca.Testes.Mocks.Servico;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Testes.Biblioteca.Apresentacao.Controllers.TurnoControllerTeste
+{
+    public partial class TurnoControllerTeste
+    {
+
+        [TestMethod]
+        public async Task Obter_DeveRetornraOk()
+        {
+            var turnoRepositorioMock = new TurnoRepositorioMock()
+                .Configurar_Obter(_turnosMock)
+                .Build();
+
+            var turnoServicoMock = new TurnoServicoMock().Build();
+
+            var controller = new TurnoController(turnoRepositorioMock, turnoServicoMock);
+
+            var resultado = await controller.Obter();
+
+            Assert.IsNotNull(resultado);
+            Assert.IsInstanceOfType(resultado, typeof(OkObjectResult));
+        }
+
+        [TestMethod]
+
+        public void Obter_DeveRetornraErro500()
+        {
+            var turnoRepositorioMock = new TurnoRepositorioMock()
+                .Configurar_ObterComExcessao()
+                .Build();
+
+            var turnoServicoMock = new TurnoServicoMock().Build();
+
+            var controller = new TurnoController(turnoRepositorioMock, turnoServicoMock);
+
+            Assert.ThrowsExceptionAsync<Exception>(async () =>
+            {
+                var resultado = await controller.Obter() as ObjectResult;
+
+                Assert.IsNotNull(resultado);
+                Assert.AreEqual(resultado.StatusCode, 500);
+                Assert.IsInstanceOfType(resultado.Value, typeof(ErrorResponse));
+            });
+        }
+
+    }
+}
---
+++ Biblioteca.Apresentacao/Biblioteca.Testes/Biblioteca.Dominio/ErroResponseTeste.cs
@@ -0,0 +1,28 @@
+﻿using Biblioteca.Dominio.DTO;
+
+namespace Biblioteca.Testes.Biblioteca.Dominio
+{
+    [TestClass]
+    public class ErroResponseTeste
+    {
+        [TestMethod]
+        public void DeveAtribuirErro()
+        {
+            var erroResponse = new ErrorResponse();
+
+            erroResponse.AtribuirErro("nome", "nome ja existe");
+            erroResponse.AtribuirErro("nome", "nome não pode conter espaços em branco");
+            erroResponse.AtribuirErro("id", "id invalido");
+
+
+            Assert.IsTrue(erroResponse.Erros.ContainsKey("nome"));
+            Assert.IsTrue(erroResponse.Erros["nome"].Count == 2);
+            Assert.IsTrue(erroResponse.Erros["nome"][0].Equals("nome ja existe"));
+            Assert.IsTrue(erroResponse.Erros["nome"][1].Equals("nome não pode conter espaços em branco"));
+
+            Assert.IsTrue(erroResponse.Erros.ContainsKey("id"));
+            Assert.IsTrue(erroResponse.Erros["id"].Count == 1);
+            Assert.IsTrue(erroResponse.Erros["id"][0].Equals("id invalido"));
+        }
+    }
+}
