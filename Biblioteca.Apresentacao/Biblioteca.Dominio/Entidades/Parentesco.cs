34fda87f0beee67269546b4fcb99ee3b707210e7
Lauro Vitor Pereira Pina <laurovpina@gmail.com>
2023-11-17 15:03:38 -0300

    Adicionando Entidade Parentesco

---
+++ Biblioteca.Apresentacao/Biblioteca.Dominio/Entidades/Parentesco.cs
@@ -0,0 +1,37 @@
+﻿using Biblioteca.Dominio.Servico;
+
+namespace Biblioteca.Dominio.Entidades
+{
+    public class Parentesco
+    {
+        private Guid _idParentesco;
+
+        private string? _nome;
+
+        public virtual ICollection<AlunoContato>? AlunoContatos { get; set; }
+
+        public Guid IdParentesco
+        {
+            get
+            {
+                return _idParentesco;
+            }
+            set
+            {
+                _idParentesco = ValidacaoServico.ValidarId("idParentesco", value);
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome ?? string.Empty;
+            }
+            set
+            {
+                _nome = ValidacaoServico.ValidarNome("nome", value);
+            }
+        }
+    }
+}
---
+++ Biblioteca.Apresentacao/Biblioteca.Repositorio/EntityFramework/ParentescoEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Biblioteca.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Biblioteca.Repositorio.EntityFramework
+{
+    internal class ParentescoEntityTypeConfiguration : IEntityTypeConfiguration<Parentesco>
+    {
+        public void Configure(EntityTypeBuilder<Parentesco> builder)
+        {
+            builder.HasKey(p => p.IdParentesco);
+        }
+    }
+}
