f3ac7aca8b71184394302d5743cbe31246d11cbb
Lauro Vitor Pereira Pina <lauro.pina@globalsys.com.br>
2023-11-24 19:03:13 -0300

    Adicionando policy de CORS

--- Biblioteca.Apresentacao/Biblioteca.Apresentacao/Controllers/Views/LivroController.cs
+++ Biblioteca.Apresentacao/Biblioteca.Apresentacao/Controllers/Views/LivroController.cs
@@ -5,6 +5,7 @@ namespace Biblioteca.Apresentacao.Controllers.Views
     [Route("views/livro")]
     public class LivroController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
